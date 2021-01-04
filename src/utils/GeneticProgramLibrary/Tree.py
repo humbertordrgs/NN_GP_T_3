e31f169db237936776fd5d427e4f1b0f91c47dea
Humberto Rodrigues <humberto.rdrgs@gmail.com>
2021-01-04 14:03:54 -0300

    fixing post order

--- src/utils/GeneticProgramLibrary/Tree.py
+++ src/utils/GeneticProgramLibrary/Tree.py
@@ -125,6 +125,6 @@ class Tree():
     if not self.is_leaf:
       res_l = self.left.eval_post_order()
       res_r = self.right.eval_post_order()
-      res = f"{self.value.__name__ if self.value.__name__ else self.value.__str__()}({res_l}, {res_r})"      
+      res = f"{getattr(self.value,"name",self.value.__str__())}({res_l}, {res_r})"      
       return res
     return str(self.value)
